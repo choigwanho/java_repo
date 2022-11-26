b05fd729f0153b9b20772d1b16a63e7c9d389e6f
dev-choee <rhksgh99@gmail.com>
2022-11-26 22:19:37 +0900

    22.11.26

---
+++ BOJ/15649.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+    public static int[] arr;
+    public static boolean[] vis;
+    public static void dfs(int n, int m, int d){
+
+        // 깊이가 m이면 리턴
+        if (d==m){
+            for(int val : arr){
+                System.out.println(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            if (!vis[i]){
+                vis[i]=true;
+                arr[d]=i+1;
+                dfs(n,m,d+1);
+                vis[i]=false;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 1~8까지의 자연수 n,m 입력. 1부터 n까지 자연수 중에서 중복 없이 m개를 고른 수열
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        vis = new boolean[n];
+        dfs(n,m,0);
+    }
+}
